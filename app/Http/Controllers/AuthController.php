2f882bfe33bf3358939a308a212d47378686c2e3
Kenyi Valladolid <valladolidk03@gmail.com>
2025-10-01 00:17:53 -0500

    :rocket: Update system

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Auth\LoginRequest;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(LoginRequest $request)
+
+    public function login(LoginRequest $request): JsonResponse
     {
         $token = $request->authenticate();
         return response()->json([
