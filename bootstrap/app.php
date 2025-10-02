37eb82437e241554d8d96c3d8456174d7a937b55
Kenyi Valladolid <valladolidk03@gmail.com>
2025-10-02 18:08:44 -0500

    :rocket: Update create view migrate

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,8 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\HandleCors;
 
-$app = Application::configure(basePath: dirname(__DIR__))
+$basePath = dirname(__DIR__);
+$app = Application::configure(basePath: $basePath)
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
