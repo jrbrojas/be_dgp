931ea597fbea1beb1cb5666786a6a83f4fc8b0e4
Kenyi Valladolid <valladolidk03@gmail.com>
2025-10-03 14:40:05 -0500

    :rocket: Update system

--- config/cors.php
+++ config/cors.php
@@ -2,10 +2,10 @@
 
 return [
     'paths'                => ['api/*', 'storage/*'],
-    'allowed_methods'      => ['*'],
+    'allowed_methods'      => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     'allowed_origins'      => explode(',', env('CORS_ALLOWED_ORIGINS', '*')),
     'allowed_headers'      => ['*'],
-    'exposed_headers'      => [],
+    'exposed_headers'      => ['Content-Disposition'],
     'max_age'              => 3600,
     'supports_credentials' => false,
 ];
